16a9a9b41ec206799f1ba454b35e7a4a2491739b
Polak Das Shawon <pulok3968@gmail.com>
2022-07-07 00:38:03 +0600

    delete the query limit

--- views.py
+++ views.py
@@ -1,9 +1,12 @@
+import greetings
 from models import Log
 
+
+greetings.show_greetings('Daily Log')
 
 def create_log(message):
     Log.create(message=message)
 
 
 def get_logs():
-    return Log.select().order_by(Log.timestamp.asc()).limit(10)
+    return Log.select().order_by(Log.timestamp.asc())
