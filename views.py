cc7a79474f5932a83564d1652a664953391ea7ce
polakD <pulok3968@gmail.com>
2022-07-09 22:39:56 +0600

    get and set log messages

--- views.py
+++ views.py
@@ -1,5 +1,5 @@
 import header
-from models import Log
+from models import Log, PositiveLog, NegativeLog
 
 
 header.show_title('Daily Log')
@@ -8,5 +8,21 @@ def create_log(message):
     Log.create(message=message)
 
 
+def create_positive_log(title, description):
+    PositiveLog.create(title=title, description=description)
+
+
+def create_negative_log(title, description):
+    NegativeLog.create(title=title, description=description)
+
+
 def get_logs():
     return Log.select().order_by(Log.timestamp.asc())
+
+
+def get_positive_log():
+    return PositiveLog.select().order_by(PositiveLog.timestamp.desc())
+
+
+def get_negative_log():
+    return NegativeLog.select().order_by(NegativeLog.timestamp.desc())
