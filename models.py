e203da9e853a08e313c44eeadac6c536f7509182
polakD <pulok3968@gmail.com>
2022-07-09 22:39:19 +0600

    add positive and negative log table

--- models.py
+++ models.py
@@ -6,3 +6,14 @@ from connection import BaseModel
 class Log(BaseModel):
     timestamp = peewee.DateTimeField(default=datetime.now)
     message = peewee.TextField()
+
+class PositiveLog(BaseModel):
+    title = peewee.CharField(max_length=100, null=True)
+    description = peewee.TextField(null=True)
+    timestamp = peewee.DateTimeField(default=datetime.now)
+
+
+class NegativeLog(BaseModel):
+    title = peewee.CharField(max_length=100, null=True)
+    description = peewee.TextField(null=True)
+    timestamp = peewee.DateTimeField(default=datetime.now)
